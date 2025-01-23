e938bc95747a1cb7827fc998dbbb32813b54ea5f
McLainCronin <cronin97@gmail.com>
2025-01-23 15:49:47 -0800

    Add data extraction method to NYCTaxiExtractor for specified date range

--- airflow-docker/dags/taxi_extractor.py
+++ airflow-docker/dags/taxi_extractor.py
@@ -9,3 +9,10 @@ class NYCTaxiExtractor:
     def __init__(self, snowflake_credentials: Dict):
         self.snowflake_credentials = snowflake_credentials
         self.base_url = "https://data.cityofnewyork.us/api/views/qp3b-zxtp"
+
+    def extract_data(self, date: datetime) -> pd.DataFrame:
+         endpoint = f"/views/t7mc-8hvi/rows.json"
+         params = {
+             "$where": f"pickup_datetime between '{date.strftime('%Y-%m-%d')}' and '{(date + timedelta(days=1)).strftime('%Y-%m-%d')}'",
+             "$limit": 50000
+         }
