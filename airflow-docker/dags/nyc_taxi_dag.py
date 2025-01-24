332d61bcbcc2d2637825bd2c5c3d0bb4d47cd199
McLainCronin <cronin97@gmail.com>
2025-01-23 16:53:20 -0800

    Add NYC Taxi data pipeline DAG to Airflow for daily data extraction and loading

---
+++ airflow-docker/dags/nyc_taxi_dag.py
@@ -0,0 +1,34 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime, timedelta
+from taxi_extraction import NYCTaxiExtractor
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2024, 1, 1),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5)
+}
+
+dag = DAG(
+    'nyc_taxi_pipeline',
+    default_args=default_args,
+    description='NYC Taxi Data Pipeline',
+    schedule_interval='@daily',
+    catchup=False
+)
+
+def extract_load_taxi_data(**context):
+    execution_date = context['execution_date']
+    
+    snowflake_creds = {
+        'account': '{{ conn.snowflake_default.account }}',
+        'user': '{{ conn.snowflake_default.login }}',
+        'password': '{{ conn.snowflake_default.password }}',
+        'warehouse': 'TAXI_WH',
+        'database': 'NYC_TAXI_PIPELINE',
+        'schema': 'RAW'
+    }
